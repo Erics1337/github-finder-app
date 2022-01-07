328a4e14ab11ea6443b51fd24915e8c54e4b200a
Eric <erics1337@gmail.com>
2022-01-06 20:54:47 -0700

    Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,7 +12,12 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main className='container mx-auto px-3 pb-12'>
-          Content
+          <Routes >
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
       </div>
